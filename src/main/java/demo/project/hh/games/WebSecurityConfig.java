b2a2caa6e73854b13fca82a03b068737095ef721
tommygratschew <tommy.gratschew@myy.haaga-helia.fi>
2020-11-17 19:04:59 +0200

    permission changes & small fixes

--- src/main/java/demo/project/hh/games/WebSecurityConfig.java
+++ src/main/java/demo/project/hh/games/WebSecurityConfig.java
@@ -22,7 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
+        .authorizeRequests().antMatchers("/css/**","/gamelist").permitAll() // Enable css & gamelist when logged out
         .and()
         .authorizeRequests()
         .anyRequest().authenticated()
